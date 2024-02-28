aed82435672b6927e019b4ce800388811ca35fcc
valenjb <valenjimenob@gmail.com>
2024-02-27 21:45:21 -0300

    cambio de app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-app.use(express.static("public"));
-app.listen(3000, () => {console.log("servidor corriendo en el puerto 3000");});
+const path = require("path");
+
+app.use (express.static("public"));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto${port}`));
+
 app.get("/", (req, res) => {res.sendFile(path.resolve(__dirname, "./views/home.html"));});
 app.get("/register", (req, res) => {res.sendFile(path.resolve(__dirname, "./views/register.html"));});
 app.get("/login", (req, res) => {res.sendFile(path.resolve(__dirname, "./views/login.html"));});
